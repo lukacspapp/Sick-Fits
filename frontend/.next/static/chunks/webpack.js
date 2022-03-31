1b36024b0991a937c94448d5967b9c73a550b0a3
lukacspapp <papplukacs@hotmail.com>
2022-03-31 23:59:20 +0100

    moving to frontend

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6828774d884cf011aed4"; }
+/******/ 		__webpack_require__.h = function() { return "836179b8e411436f99ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
