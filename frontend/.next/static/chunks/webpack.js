31c7a702bc3916a79cd524b63139e8cb286d9679
lukacspapp <papplukacs@hotmail.com>
2022-04-13 14:49:49 +0100

    updating a product has started

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5e0bc6eea7cee17c880"; }
+/******/ 		__webpack_require__.h = function() { return "e63b5f6c45d673118170"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/.next/static/webpack/webpack.6a47ff618a7769e80a1f.hot-update.js
+++ frontend/.next/static/webpack/webpack.6a47ff618a7769e80a1f.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "eaa89c52cf6534b7125f"; }
+/******/ 	__webpack_require__.h = function() { return "9e7092b294a9f3122f57"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
